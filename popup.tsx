7ad2e7c5b4d228cbff7b9b46fceade951e4eb4d0
Kai yang <kaiyang939325@gmail.com>
2024-04-14 23:31:31 +0800

    remove useless code

--- popup.tsx
+++ popup.tsx
@@ -1,17 +1,1 @@
-import "./style.css"
-
-export default function IndexPopup() {
-  const openSidebar = () => {
-    chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true })
-  }
-  return <></>
-
-  // return (
-  //   <div className="w-[300px] flex flex-col items-center justify-center p-4">
-  //     <p className="text-gray-600 mb-2 text-sm">
-  //       {chrome.i18n.getMessage("popupHint")}
-  //     </p>
-  //     <button onClick={openSidebar}>Open Sidebar</button>
-  //   </div>
-  // )
-}
+chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true })
