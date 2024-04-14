d10a6478b80d1e136916143a75ca416b11164089
Kai yang <kaiyang939325@gmail.com>
2024-04-14 17:37:47 +0800

    change launch

--- popup.tsx
+++ popup.tsx
@@ -1,11 +1,17 @@
 import "./style.css"
 
 export default function IndexPopup() {
-  return (
-    <div className="w-[300px] flex flex-col items-center justify-center p-4">
-      <p className="text-gray-600 mb-2 text-sm">
-        {chrome.i18n.getMessage("popupHint")}
-      </p>
-    </div>
-  )
+  const openSidebar = () => {
+    chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true })
+  }
+  return <></>
+
+  // return (
+  //   <div className="w-[300px] flex flex-col items-center justify-center p-4">
+  //     <p className="text-gray-600 mb-2 text-sm">
+  //       {chrome.i18n.getMessage("popupHint")}
+  //     </p>
+  //     <button onClick={openSidebar}>Open Sidebar</button>
+  //   </div>
+  // )
 }
